0c378cfdaae17cc15c9e9b8ab003ae97385c7eda
Jingren XIE <33126026+ZekiFayes@users.noreply.github.com>
2019-08-29 10:47:28 +0800

    Add files via upload

---
+++ DL4CV/iudlm/preprocessor/imagetoarray.py
@@ -0,0 +1,12 @@
+from keras.preprocessing.image import img_to_array
+
+
+class ImageToArray(object):
+
+    def __init__(self, dataFormat=None):
+
+        self.dataFormat = dataFormat
+
+    def preprocess(self, image):
+
+        return img_to_array(image, data_format=self.dataFormat)
---
+++ DL4CV/iudlm/preprocessor/meanpreprocessor.py
@@ -0,0 +1,19 @@
+import cv2
+
+
+class MeanPreprocessor(object):
+
+    def __init__(self, rMean, gMean, bMean):
+
+        self.rMean = rMean
+        self.gMean = gMean
+        self.bMean = bMean
+
+    def preprocess(self, image):
+
+        (B, R, G) = cv2.split(image.astype("float32"))
+        R -= self.rMean
+        G -= self.gMean
+        B -= self.bMean
+
+        return cv2.merge([B, G, R])
