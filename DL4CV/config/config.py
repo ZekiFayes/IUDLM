aa86a2c3654daf9095d6080a25525ffa999a552a
Jingren XIE <33126026+ZekiFayes@users.noreply.github.com>
2019-08-29 10:35:14 +0800

    Add files via upload

---
+++ DL4CV/config/config.py
@@ -0,0 +1,12 @@
+"""
+This is to define the paths.
+"""
+IMAGE_PATH = "dataset_model_fig/dataset/train"
+
+TRAIN_PATH = "dataset_model_fig/dataset/train.hdf5"
+TEST_PATH = "dataset_model_fig/dataset/test.hdf5"
+MEAN_PATH = "dataset_model_fig/dataset/mean.json"
+
+TEST_SIZE = 6000
+
+MODEL_PATH = "dataset_model_fig/model/alexnet.model"
